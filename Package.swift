3746801b0edb09a9dbda3750f65e3ea05378e529
Brian Dupras <brian.dupras@duprasville.com>
2026-01-31 18:01:08 -0800

    use Xcode for all app builds (SPM doesn't support mixed Swift/ObjC). keep SPM for building tests

--- Package.swift
+++ Package.swift
@@ -7,14 +7,15 @@ let package = Package(
         .macOS(.v13)
     ],
     products: [
-        .executable(name: "Tabzilla", targets: ["Tabzilla"])
+        // Library for testing (executable is built via Xcode, not SPM)
+        .library(name: "Tabzilla", targets: ["Tabzilla"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/jpsim/Yams", from: "5.1.0")
     ],
     targets: [
-        .executableTarget(
+        .target(
             name: "Tabzilla",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
@@ -23,7 +24,17 @@ let package = Package(
             path: "Sources",
             exclude: [
                 "Resources/Info.plist",
-                "Resources/Tabzilla.entitlements"
+                "Resources/Tabzilla.entitlements",
+                // Exclude Objective-C and dependent files (SPM doesn't support mixed Swift/ObjC)
+                // Full app builds use Xcode which includes these files
+                "Chrome.h",
+                "ChromeController.h",
+                "ChromeController.m",
+                "Tabzilla-Bridging-Header.h",
+                "Executor.swift",
+                // These depend on Executor, so also excluded
+                "CLI.swift",
+                "TabzillaApp.swift"
             ],
             resources: [
                 .process("Resources/DefaultConfig.yaml")
